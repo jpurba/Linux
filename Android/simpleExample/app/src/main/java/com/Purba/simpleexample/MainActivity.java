7d2b397ce1b8604f65a90bed9b52564f625f71ee
jpurba <jpurba@ieee.org>
2020-08-03 16:08:45 -0700

    update android simpleExample with Java
    update android simpleExample with Java

--- Android/simpleExample/app/src/main/java/com/Purba/simpleexample/MainActivity.java
+++ Android/simpleExample/app/src/main/java/com/Purba/simpleexample/MainActivity.java
@@ -3,6 +3,9 @@ package com.Purba.simpleexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View rollButton = findViewById(R.id.rollButton);
+        View resultsTextView = findViewById(R.id.resultsTextView);
+        View seekBar = findViewById(R.id.seekBar);
+
+        rollButton.setOnClickListener(it:View!
+        int rand = Random().nextInt(seekBar.progr)
+        );
     }
 }
